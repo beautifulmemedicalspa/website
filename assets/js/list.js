c50c0b3f314c36cd3ada9435ef19ea15c9fb2645
beautifulmemedicalspa <beautifulmemedicalspa@gmail.com>
2023-12-23 19:23:16 -0800

    Transformation photos edit

--- assets/js/list.js
+++ assets/js/list.js
@@ -13,75 +13,101 @@ function toggleSubContent(element) {
 	}
 }
 
-const state = {};
-const carouselList = document.querySelector(".carousel__list");
-const carouselItems = document.querySelectorAll(".carousel__item");
-const elems = Array.from(carouselItems);
+//const state = {};
+//const carouselList = document.querySelector(".carousel__list");
+//const carouselItems = document.querySelectorAll(".carousel__item");
+//const elems = Array.from(carouselItems);
 
 
-const update = function (newActive) {
-	const newActiveIndex = elems.indexOf(newActive);
+//const update = function (newActive) {
+//	const newActiveIndex = elems.indexOf(newActive);
 
-	elems.forEach((item, index) => {
-		// Remove the active class from all items
-		item.classList.remove("carousel__item_active");
+//	elems.forEach((item, index) => {
+//		// Remove the active class from all items
+//		item.classList.remove("carousel__item_active");
 
-		// Update the position
-		const position = index - newActiveIndex;
+//		// Update the position
+//		const position = index - newActiveIndex;
 
-		// Set the new position
-		item.dataset.pos = position;
+//		// Set the new position
+//		item.dataset.pos = position;
 
-		// Add active class to the new active item
-		if (position === 0) {
-			item.classList.add("carousel__item_active");
+//		// Add active class to the new active item
+//		if (position === 0) {
+//			item.classList.add("carousel__item_active");
+//		}
+//	});
+//};
+//const getPos = function (current, active) {
+//	const diff = current - active;
+
+//	if (Math.abs(current - active) > 2) {
+//		return -current;
+//	}
+
+//	return diff;
+//};
+//// Function to find the index of the current active item
+//const findActiveIndex = (items) => {
+//	for (let i = 0; i < items.length; i++) {
+//		if (items[i].classList.contains("carousel__item_active")) {
+//			return i;
+//		}
+//	}
+//	return -1; // Return -1 if no active item is found
+//};
+
+
+//// Function to advance the carousel to the next item
+//const advanceCarousel = function () {
+//	try {
+//		console.log("Advancing carousel"); // Log to track execution
+//		const currentIndex = findActiveIndex(elems);
+//		let nextIndex = currentIndex + 1;
+
+//		// If at the end, loop back to the first item
+//		if (nextIndex >= elems.length) {
+//			nextIndex = 0;
+//		}
+
+//		const newActive = elems[nextIndex];
+//		console.log(`Current index: ${currentIndex}, Next index: ${nextIndex}`); // Log indexes
+//		update(newActive);
+//	} catch (error) {
+//		console.error("Error in advanceCarousel:", error); // Log any errors
+//	}
+//};
+
+//// Ensure the first item is active initially
+//if (elems.length > 0 && findActiveIndex(elems) === -1) {
+//	elems[0].classList.add("carousel__item_active");
+//}
+
+//// Automatically advance the carousel every 3 seconds (3000 milliseconds)
+//setInterval(advanceCarousel, 5000);
+
+
+	const slider = $('.slider');
+	const slideWidth = $('.slide').outerWidth();
+	let currentPosition = 0;
+
+	function autoScroll() {
+		currentPosition -= slideWidth;
+		if (currentPosition < -slideWidth * ($('.slide').length - 1)) {
+			currentPosition = 0;
 		}
-	});
-};
-const getPos = function (current, active) {
-	const diff = current - active;
-
-	if (Math.abs(current - active) > 2) {
-		return -current;
+		slider.css('transform', `translateX(${currentPosition}px)`);
 	}
 
-	return diff;
-};
-// Function to find the index of the current active item
-const findActiveIndex = (items) => {
-	for (let i = 0; i < items.length; i++) {
-		if (items[i].classList.contains("carousel__item_active")) {
-			return i;
+	// Set an interval for autoscrolling (e.g., every 3 seconds)
+	const autoScrollInterval = setInterval(autoScroll, 3000);
+
+	// Stop autoscrolling on hover
+	$('.slider-container').hover(
+		function () {
+			clearInterval(autoScrollInterval);
+		},
+		function () {
+			autoScrollInterval = setInterval(autoScroll, 3000);
 		}
-	}
-	return -1; // Return -1 if no active item is found
-};
-
-
-// Function to advance the carousel to the next item
-const advanceCarousel = function () {
-	try {
-		console.log("Advancing carousel"); // Log to track execution
-		const currentIndex = findActiveIndex(elems);
-		let nextIndex = currentIndex + 1;
-
-		// If at the end, loop back to the first item
-		if (nextIndex >= elems.length) {
-			nextIndex = 0;
-		}
-
-		const newActive = elems[nextIndex];
-		console.log(`Current index: ${currentIndex}, Next index: ${nextIndex}`); // Log indexes
-		update(newActive);
-	} catch (error) {
-		console.error("Error in advanceCarousel:", error); // Log any errors
-	}
-};
-
-// Ensure the first item is active initially
-if (elems.length > 0 && findActiveIndex(elems) === -1) {
-	elems[0].classList.add("carousel__item_active");
-}
-
-// Automatically advance the carousel every 3 seconds (3000 milliseconds)
-setInterval(advanceCarousel, 5000);
+	);
