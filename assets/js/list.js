9707f8b58c2cfec5ff50af7d54c3ea66e1a6a24c
beautifulmemedicalspa <beautifulmemedicalspa@gmail.com>
2024-01-09 16:11:42 -0800

    Slider updates

--- assets/js/list.js
+++ assets/js/list.js
@@ -1,24 +1,19 @@
-const slider = $(".slider");
-const slideWidth = $(".slide").outerWidth();
-let currentPosition = 0;
+let slideIndex = 0;
+showSlides();
 
-function autoScroll() {
-	currentPosition -= slideWidth;
-	if (currentPosition < -slideWidth * ($(".slide").length - 1)) {
-		currentPosition = 0;
-	}
-	slider.css("transform", `translateX(${currentPosition}px)`);
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}    
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" slideractive", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " slideractive";
+  setTimeout(showSlides, 5000); // Change image every 3 seconds
 }
-
-// Set an interval for autoscrolling (e.g., every 3 seconds)
-const autoScrollInterval = setInterval(autoScroll, 5000);
-
-// Stop autoscrolling on hover
-$(".slider-container").hover(
-	function () {
-		clearInterval(autoScrollInterval);
-	},
-	function () {
-		autoScrollInterval = setInterval(autoScroll, 5000);
-	}
-);
