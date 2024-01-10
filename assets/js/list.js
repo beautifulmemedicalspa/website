60cd079a50cdbba39766c6ff075aa087dd285738
beautifulmemedicalspa <beautifulmemedicalspa@gmail.com>
2024-01-09 21:08:27 -0800

    Toggle content

--- assets/js/list.js
+++ assets/js/list.js
@@ -17,3 +17,17 @@ function showSlides() {
   dots[slideIndex-1].className += " slideractive";
   setTimeout(showSlides, 5000); // Change image every 3 seconds
 }
+function toggleSubContent(element) {
+	var subContent = element.querySelector(".sub-content");
+	var dropdownIndicator = element.querySelector(".dropdown-indicator");
+
+	if (subContent.style.display === "none") {
+		subContent.style.display = "block";
+		dropdownIndicator.style.transform = "rotate(180deg)";
+		element.classList.add("active");
+	} else {
+		subContent.style.display = "none";
+		dropdownIndicator.style.transform = "rotate(0deg)";
+		element.classList.remove("active");
+	}
+}
